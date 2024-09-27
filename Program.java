59d74dffb9b1b75714ec5d0f367b573de9cf6fc9
Arvind Kumar <arvind2341989@gmail.com>
2024-09-27 11:22:32 +0530

    Added classes for addition, division, subtraction, multiplication and program.

---
+++ Program.java
@@ -0,0 +1,32 @@
+class Program {
+	public static void main(String[] args) {
+		/*add();
+		subtract();
+		divide();
+		multiply();*/
+		
+		/*new Addition().add();
+		new Division().divide();
+		new Subtraction().subtract();
+		new Multiplication().multiply();*/
+		
+				 //REFERENCE VARIABLE/OBJECT
+		/*Addition aobj = new Addition();
+		aobj.add();
+		Division dobj = new Division();
+		dobj.divide();
+		Subtraction sobj = new Subtraction();
+		sobj.subtract();
+		Multiplication mobj = new Multiplication();
+		mobj.multiply();*/
+		
+		Addition addition = new Addition();
+		addition.add();
+		Division division = new Division();
+		division.divide();
+		Subtraction subtraction = new Subtraction();
+		subtraction.subtract();
+		Multiplication multiplication = new Multiplication();
+		multiplication.multiply();
+	}
+}
