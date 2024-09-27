c8ea3423890ac06f108f026540a7f0c67b8af1c7
roshani <roshanivishvakarma60@gmail.com>
2024-09-27 22:00:38 +0530

    developer message

---
+++ Program.java
@@ -0,0 +1,14 @@
+class Program {
+  public static void main(String[] args) {
+
+    Addition aobj = new Addition();
+    aobj.add();
+    Division dobj = new Division();
+    dobj.divide();
+    Multiplication mobj = new Multiplication();
+    mobj.multiply();
+    Substraction sobj = new Substraction();
+    sobj.substract();
+
+  }
+}
